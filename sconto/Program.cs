53fafdb1684e5be19b8bf4a5a53857373a3533d4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 13:04:12 +0200

    sconto

---
+++ sconto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace sconto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sconto = 0;
+            Console.Write("Inserisci l' importo della spesa: ");
+            double importo = double.Parse(Console.ReadLine());
+            if (importo < 500)
+            {
+                sconto = (importo / 100) * 10;
+                importo = importo - sconto;
+            }
+            else if (importo >= 500)
+            {
+                sconto = (importo / 100) * 20;
+                importo = importo - sconto;
+            }
+            Console.Write($"L'importo scontato è {importo:c} e lo sconto è {sconto:c}");
+            Console.ReadLine();
+        }
+    }
+}
